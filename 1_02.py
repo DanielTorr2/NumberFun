c54b5df7c414f4f6cecf56d5e21578b27a473551
Danieltorr2 <dandoesgrowth@gmail.com>
2025-01-02 20:34:37 -0600

    A bit of number fun, relearned function declarations, variable initialization, and formatting

--- 1_02.py
+++ 1_02.py
@@ -1,1 +1,30 @@
-sss
+import numpy
+
+
+
+#Goal: Multiply 3^{3}, 9^{3}, 27^{3}
+
+def cube(a):
+  return a**3
+
+def multiplyCubes(a, b, c):
+  return cube(a)*cube(b)*cube(c)
+
+print(multiplyCubes(3, 9, 27))
+
+
+#70% of 4mill, 300/month, * 12
+
+def insuranceProfit():
+  rate = 300
+  pop = 4000000
+  insured_pop = pop * 0.7
+
+  profit = insured_pop * rate
+  return profit
+
+def yearlyInsuranceProfit():
+  return insuranceProfit()*12
+
+
+print(f"{yearlyInsuranceProfit():,}")
